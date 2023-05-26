fca3c976a4e0ddfc185926d0119fc4027658864f
hamza ali <tanolihamzaali@gmail.com>
2023-05-26 17:56:56 +0500

    feat: card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,62 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import DemoImage from 'src/assets/demo-book.jpg';
+import Pattren from 'src/assets/pattren.jpg';
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	return <h1 className="text-[red]">Kitabcha</h1>;
+	return (
+		<div className="w-screen h-screen flex justify-center bg-main">
+			<div className="w-[80%] h-screen items-center flex">
+				<div className="card relative flex flex-col justify-between bg-[#DEE7F750] h-[500px] w-[350px] shadow-lg">
+					<div className="w-full h-[40%]">
+						<img src={Pattren} className="w-full h-full object-cover opacity-80" />
+					</div>
+					<div className="absolute top-[20%] left-5 right-5 flex justify-between items-end">
+						<img
+							src={DemoImage}
+							className=" object-cover w-[100px] h-[120px] shadow-sm shadow-black"
+						/>
+						<div className="flex mb-3 h-max gap-3">
+							<div className="w-[20px] flex items-center justify-center rounded-full h-[20px] bg-white shadow-lg">
+								s
+							</div>
+							<button className="bg-[#F15A29] text-[12px] text-white px-2 rounded-full shadow-lg">
+								Read
+							</button>
+						</div>
+					</div>
+					<div className="flex h-[50px] mx-5 shadow-lg px-4 justify-between bg-white">
+						<Item count={1023} title="Likes" />
+						<Item count={1023} title="Likes" />
+						<Item count={1023} title="Likes" />
+						<Item count={1023} title="Likes" />
+					</div>
+					<div className="max-h-[35%] items-between min-h-max shadow-lg mx-5 py-4 bg-white mb-2 flex flex-col overflow-hidden px-4">
+						<h3 className="font-bold text-[14px]">Synopsis</h3>
+						<p className="text-[12px]">
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+							Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+							when an unknown printer took a galley of type and scrambled it to make a type
+							specimen book. It has survived not only five centuries.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
+
+interface IItem {
+	count: number;
+	title: string;
+}
+
+const Item = ({ count, title }: IItem) => {
+	return (
+		<div className="flex flex-col items-center justify-center">
+			<h6 className="font-bold text-[12px]">{count}</h6>
+			<p className="-mt-1 text-[12px] text-main text-[#231F2080]">{title}</p>
+		</div>
+	);
+};

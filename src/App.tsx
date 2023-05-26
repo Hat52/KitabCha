9a2ef591cc6ceae01a72ddd64b84de0dec38f564
hamza ali <tanolihamzaali@gmail.com>
2023-05-26 20:02:34 +0500

    feat: button fixed

--- src/App.tsx
+++ src/App.tsx
@@ -31,10 +31,18 @@ const Card = () => {
 					className=" object-cover w-[100px] h-[120px] shadow-sm shadow-black"
 				/>
 				<div className="flex mb-3 h-max gap-3">
-					<div className="w-[20px] flex items-center justify-center rounded-full h-[20px] bg-white shadow-lg">
-						s
+					<div className="w-[30px] h-[30px] flex items-center justify-center rounded-full bg-white shadow-lg">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="16"
+							height="16"
+							fill="currentColor"
+							className="bi bi-bookmark"
+							viewBox="0 0 16 16">
+							<path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z" />{' '}
+						</svg>
 					</div>
-					<button className="bg-[#F15A29] text-[12px] text-white px-2 rounded-full shadow-lg">
+					<button className="bg-[#F15A29] text-[12px] text-white px-5 py-1 rounded-full shadow-lg">
 						Read
 					</button>
 				</div>

cb64caefd1ee58d3f9daf7fcf840d2b3c486e459
hamza ali <tanolihamzaali@gmail.com>
2023-05-27 19:22:10 +0500

    auth container

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,3 +1,13 @@
+import LoginImage from 'src/assets/login.jpg';
+import Logo from 'src/assets/logo.png';
+
 export default function Login() {
-	return <h1>Login</h1>;
+	return (
+		<div className="flex w-screen h-screen">
+			<div className="w-[30%] flex flex-col pt-5 items-center h-full bg-white shadow-lg">
+				<img src={Logo} className="w-[150px] h-[150px]" />
+			</div>
+			<img src={LoginImage} className="w-[70%] object-cover h-full" />
+		</div>
+	);
 }

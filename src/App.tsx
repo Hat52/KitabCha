f0a3ada2eceea19f8e8988aa16914d0ae80660e1
hamza ali <tanolihamzaali@gmail.com>
2023-05-27 16:02:22 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Navbar } from './components';
 
 function App() {
 	return (
-		<div className="w-screen max-h-max min-h-screen flex justify-center bg-main pt-20">
+		<div className="w-screen max-h-max min-h-screen flex justify-center bg-white pt-20">
 			<Navbar />
 			<Landing />
 		</div>
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -2,7 +2,7 @@ import { Card } from '../../components';
 
 export default function Landing() {
 	return (
-		<div className="w-[75%] min-h-screen max-h-max gap-10 grid justify-between grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+		<div className="w-[75%] min-h-screen overflow-x-hidden max-h-max mt-[30px] pb-10 gap-10 grid justify-between grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
 			<Card />
 			<Card />
 			<Card />

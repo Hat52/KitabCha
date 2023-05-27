b8e8b507cd470c050538417c8364b6a457b70f07
hamza ali <tanolihamzaali@gmail.com>
2023-05-27 19:03:40 +0500

    login page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { ErrorPage, Landing } from './pages';
 import { Navbar } from './components';
+import Login from './pages/Auth/Login';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
@@ -8,13 +9,16 @@ const router = createBrowserRouter([
 		path: '/',
 		element: <Landing />,
 		errorElement: <ErrorPage />
+	},
+	{
+		path: '/login',
+		element: <Login />
 	}
 ]);
 
 function App() {
 	return (
-		<div className="w-screen max-h-max overflow-hidden min-h-screen flex justify-center bg-white pt-20">
-			<Navbar />
+		<div className="w-screen max-h-max overflow-hidden min-h-screen flex justify-center bg-white">
 			<RouterProvider router={router} />
 		</div>
 	);
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,14 +1,17 @@
-import { Card } from '../../components';
+import { Card, Navbar } from '../../components';
 
 export default function Landing() {
 	return (
-		<div className="min-h-screen max-h-max mt-[30px] pb-10 gap-10 grid justify-between grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-		</div>
+		<>
+			<Navbar />
+			<div className="min-h-screen max-h-max mt-[110px] pb-10 gap-10 grid justify-between grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
+		</>
 	);
 }

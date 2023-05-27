02f639329809debcc4d7472d8da0cfbdbea0c6de
hamza ali <tanolihamzaali@gmail.com>
2023-05-27 17:00:11 +0500

    route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css';
 import { Landing } from './pages';
 import { Navbar } from './components';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Landing />
+		// errorElement: <ErrorPage />,
+	}
+]);
 
 function App() {
 	return (
 		<div className="w-screen max-h-max overflow-hidden min-h-screen flex justify-center bg-white pt-20">
 			<Navbar />
-			<Landing />
+			<RouterProvider router={router} />
 		</div>
 	);
 }

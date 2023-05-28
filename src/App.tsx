b3beb785305fcf3055b4dbe4172e0de77e1890c5
hamza ali <tanolihamzaali@gmail.com>
2023-05-28 18:07:04 +0500

    feat: Sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ErrorPage, Landing } from './pages';
 import { Navbar } from './components';
 import Login from './pages/Auth/Login';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Signup from './pages/Auth/Signup';
 
 const router = createBrowserRouter([
 	{
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
 	{
 		path: '/login',
 		element: <Login />
+	},
+	{
+		path: '/signup',
+		element: <Signup />
 	}
 ]);
 

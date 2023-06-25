254633f1e393712c216dd325ea2f61a0606125c0
keenemik <mikkee820@gmail.com>
2023-06-25 16:40:12 +0500

    couter card

---
+++ src/components/CounterCard/index.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+interface ICounter {
+	label: string;
+	total: number;
+}
+
+export default function Counter({ label, total }: ICounter) {
+	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		if (count < total) {
+			setCount(count + 1);
+		}
+	}, [count]);
+
+	return (
+		<div className="flex flex-col items-center">
+			<h6 className="text-[40px] text-[#F15A29] font-black">{count}</h6>
+			<p className="text-[20px]">{label}</p>
+		</div>
+	);
+}
